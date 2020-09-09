b7f5eab8021e3fb8ce2ec1893dab8b3263deefd9
Rodrigo M.P Andrade <rodrigo.andrade@squadra.com.br>
2020-09-08 22:24:14 -0300

    UPDATE - Initial Commit: Estrutura basica criada

---
+++ calculator/src/App.java
@@ -0,0 +1,42 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Classe principal
+ * 
+ * @author Rodrigo Andrade
+ */
+public class App extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final int WIDTH = 350;
+    public static final int HEIGHT = 450;
+
+    /**
+     * Construtor
+     * 
+     * @param entryPoint Painel inicial da aplicacao.
+     */
+    public App(final JPanel entryPoint) {
+        super("Calculadora");
+        this.setMinimumSize(new Dimension(App.WIDTH, App.HEIGHT));
+        this.setDefaultCloseOperation(App.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().add(entryPoint);
+        this.pack();
+    }   
+
+    /**
+     * Metodo de inicializacao
+     */
+    public void start() {
+        this.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new App(null).start();
+    }
+}
