9d1fdcf610c9790ab95d56e93d112a62c66353b1
Rodrigo M.P Andrade <rodrigo.andrade@squadra.com.br>
2020-09-16 21:36:41 -0300

    REFACOTRING - Renomeando variaveis

--- calculator/src/utils/Constants.java
+++ calculator/src/utils/Constants.java
@@ -6,11 +6,9 @@ package utils;
  * @author Rodrigo Andrade
  */
 public class Constants {
-
     public static final String DELETAR = "Deletar";
     public static final String RESULT_VALUE = "Resultado";
     public static final String DEFAULT_VALUE = "Nenhuma operação...";
     public static final String SELECT_OPERATION = "Selecionar Operação";
     public static final String DIVISION_BY_ZERO = "Não é possível dividir por zero.";
-
 }
